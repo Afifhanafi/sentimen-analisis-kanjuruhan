6c6f7f1173cee4c6df11a99720ef81e7d6547a29
Andre Kautsar <ktsr.andre@gmail.com>
2022-11-06 13:38:27 +0700

    Add database & sign up

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, template_folder="views", static_folder="static")
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:''@localhost/skripsi_afif'
+app.config["SECRET_KEY"] = '8a4434eac74c483ee6902cf12ba089fb'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_Manager = LoginManager(app)
 
 from app import routes
--- app/controllers/authcontroller.py
+++ app/controllers/authcontroller.py
@@ -1,5 +1,8 @@
+from app import db
+
 def index():
     pass
+    print("Hello World")
 def show():
     pass
 def insert():
