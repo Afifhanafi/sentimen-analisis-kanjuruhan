d2117c97cbcfd4f52be9354d34c051823b1ab0d2
Andre Kautsar <ktsr.andre@gmail.com>
2022-11-08 17:26:10 +0700

    Add dataset before kanjuruhan

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app, db
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, login_user, logout_user, current_user
-from .models import Dataset, Daftar_slangwords, Daftar_stopwords, Users
 from app.controllers.authcontroller import AuthController
 from app.controllers.slangwordcontroller import SlangwordController
+from app.controllers.datacontroller import DataController
 
 @app.route('/beranda', methods=['GET'])
 @app.route('/', methods=['GET'])
@@ -40,6 +40,7 @@ def signOutRoute():
     return redirect(url_for('signInRoute'))
 
 @app.route('/kata-slang', methods=["GET", "POST"])
+@login_required
 def slangwordsRoute():
     if request.method == "GET":
         slangwords_data = SlangwordController().retrieve()
@@ -52,6 +53,7 @@ def slangwordsRoute():
         return redirect(url_for('slangwordsRoute'))
 
 @app.route('/kata-slang/ubah', methods=["GET", "POST"])
+@login_required
 def slangwordUpdateRoute():
     if request.method == "POST":
         SlangwordController().update(request.form)
@@ -60,12 +62,28 @@ def slangwordUpdateRoute():
         return redirect(url_for('slangwordsRoute'))
 
 @app.route('/kata-slang/hapus', methods=["GET", "POST"])
+@login_required
 def slangwordDeleteRoute():
     if request.method == "POST":
         SlangwordController().delete(request.form)
         return redirect(url_for('slangwordsRoute'))
     else:
         return redirect(url_for('slangwordsRoute'))
+    
+@app.route('/api/dataset-sebelum-tragedi-kanjuruhan', methods=["GET"])
+@login_required
+def apiDatasetBeforeKanjuruhan():
+    dataset = DataController().retrieveBefore()
+    return dataset
+
+@app.route('/dataset/dataset-sebelum-tragedi-kanjuruhan', methods=["GET", "POST"])
+@login_required
+def DatasetBeforeKanjuruhan():
+    if request.method == "GET":
+        return render_template("/pages/dataset_before.html")
+    elif request.method == "POST" and request.files:
+        DataController().importDatasetBefore(request.files["fileCSV"])
+        return redirect(url_for('DatasetBeforeKanjuruhan'))
     
 @app.errorhandler(401)
 def unauthorized(error):
