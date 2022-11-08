87b98fa3e161587eb6d4d526031b7579036f223c
Andre Kautsar <ktsr.andre@gmail.com>
2022-11-08 14:18:48 +0700

    Add slangwords crud & import

--- app/controllers/slangwordcontroller.py
+++ app/controllers/slangwordcontroller.py
@@ -1,15 +1,68 @@
+from app import db
+from flask import flash
+from app.models import Daftar_slangwords
+import pandas as pd
+
 class SlangwordController:
-    def __init__():
+    def __init__(self):
         pass
     
-    def create():
-        pass
+    def importData(self, file):
+        try:
+            readCSV = pd.read_csv(file, sep=';', encoding='unicode_escape')
+            for row in range(0, len(readCSV)):
+                uploadSlangwords = Daftar_slangwords(
+                    kata_slang = readCSV["Kata_slang"][row],
+                    kata_baku = readCSV["Kata_baku"][row]
+                )
+                
+                db.session.add(uploadSlangwords)
+                db.session.commit()
+            
+            flash("The file successfully imported", "success")
+        except Exception as e:
+            flash(f"File failed to import", "danger")
+            return e
     
-    def retrieve():
-        pass
+    def create(self, *args):
+        try:
+            uploadSlangword = Daftar_slangwords(
+                kata_slang = args[0]["slangword"],
+                kata_baku = args[0]["kata_baku"]
+            )
+            
+            db.session.add(uploadSlangword)
+            db.session.commit()
+            
+            flash("The data successfully saved", "success")
+        except Exception as e:
+            flash(f"Data failed to save", "danger")
+            return e
     
-    def update():
-        pass
+    def retrieve(self):
+        data_slangwords = Daftar_slangwords.query.all()
+        return data_slangwords
     
-    def delete():
-        pass
+    def update(self, *args):
+        try:
+            slangUpdate = Daftar_slangwords.query.get_or_404(args[0]['id'])
+          
+            slangUpdate.kata_slang = args[0]['slangword']
+            slangUpdate.kata_baku = args[0]['kata_baku']
+            
+            db.session.commit()
+            flash("Data berhasil di ubah", "success")
+        except Exception as e:
+            flash("Data gagal di ubah", "danger")
+            return e
+    
+    def delete(self, *args):
+        try:
+            slangHapus = Daftar_slangwords.query.get(args[0]['id'])
+
+            db.session.delete(slangHapus)
+            db.session.commit()
+            flash("Data berhasil di hapus", "success")
+        except Exception as e:
+            flash("Data gagal di hapus", "danger")
+            return e
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, login_user, logout_user, current_user
 from .models import Dataset, Daftar_slangwords, Daftar_stopwords, Users
 from app.controllers.authcontroller import AuthController
+from app.controllers.slangwordcontroller import SlangwordController
 
 @app.route('/beranda', methods=['GET'])
 @app.route('/', methods=['GET'])
@@ -38,6 +39,38 @@ def signOutRoute():
     AuthController().signOut()
     return redirect(url_for('signInRoute'))
 
-@app.route('/kata-slang', methods=['GET'])
-def slangwordsIndex():
-    return render_template("/pages/slangwords.html")
+@app.route('/kata-slang', methods=["GET", "POST"])
+def slangwordsRoute():
+    if request.method == "GET":
+        slangwords_data = SlangwordController().retrieve()
+        return render_template("/pages/slangwords.html", slangwords_data=slangwords_data)
+    elif request.method == "POST" and request.files:
+        SlangwordController().importData(request.files["fileCSV"])
+        return redirect(url_for('slangwordsRoute'))
+    elif request.method == "POST" and request.form:
+        SlangwordController().create(request.form)
+        return redirect(url_for('slangwordsRoute'))
+
+@app.route('/kata-slang/ubah', methods=["GET", "POST"])
+def slangwordUpdateRoute():
+    if request.method == "POST":
+        SlangwordController().update(request.form)
+        return redirect(url_for('slangwordsRoute'))
+    else:
+        return redirect(url_for('slangwordsRoute'))
+
+@app.route('/kata-slang/hapus', methods=["GET", "POST"])
+def slangwordDeleteRoute():
+    if request.method == "POST":
+        SlangwordController().delete(request.form)
+        return redirect(url_for('slangwordsRoute'))
+    else:
+        return redirect(url_for('slangwordsRoute'))
+    
+@app.errorhandler(401)
+def unauthorized(error):
+    return redirect(url_for('signInRoute'))
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template("/pages/404.html")
